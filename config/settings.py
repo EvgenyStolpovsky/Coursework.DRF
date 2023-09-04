3d2cd5282078cbf230faab2f1fab20acadb42280
EvgenyS <e-stolpovski@mail.ru>
2023-09-04 18:43:38 +0300

    add

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m!2+2fevgn9lre*vjp8w*)%axooui#^2n!mi7vp4ox&k4dbp=n'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
